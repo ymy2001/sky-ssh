77577c489a342531abf82255e6a47f23538ea649
ymy2001 <2360302452@qq.com>
2023-12-25 09:35:27 +0800

    前端ui创建

--- sky-common/src/main/java/com/sky/result/PageResult.java
+++ sky-common/src/main/java/com/sky/result/PageResult.java
@@ -14,9 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PageResult implements Serializable {
-
     private long total; //总记录数
-
     private List records; //当前页数据集合
 
 }
